64652109c0277d825fe17c7de067ae808f965402
modulo1 <luis.escalada@hotmail.com>
2022-05-22 19:08:59 -0300

    guardando cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+// Importamos el core de componentes de Angular
 import { Component } from '@angular/core';
 
+//el componente es un decorada
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
