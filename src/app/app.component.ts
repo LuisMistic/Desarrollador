3ea608d27240d8105c07a6bdce569c237d72fe19
modulo1 <luis.escalada@hotmail.com>
2022-05-22 22:42:52 -0300

    primer dia angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Desarrollador';
+  public title = 'Aprendiendo a usar mi primer proyecto en Angular';
 }
